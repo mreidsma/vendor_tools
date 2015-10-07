16c6250f3c7205bfccaacb2d5e8c9fd75079428b
Matthew Reidsma <reidsmam@gvsu.edu>
2015-10-07 12:09:33 -0400

    Fleshed out bookmarklets section

---
+++ js/bookmarklet.js
@@ -0,0 +1,9 @@
+(function(){
+	var unicorn = document.createElement('img');
+	unicorn.src='http://mreidsma.github.io/vendor_tools/img/unicorn.gif';
+	unicorn.alt='Unicorn!';
+	unicorn.style.position='fixed';
+	unicorn.style.left='0';
+	unicorn.style.top='10%';
+	document.body.appendChild(unicorn);
+})();
